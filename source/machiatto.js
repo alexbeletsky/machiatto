dd04c383e4617af7daf33b906c0282aac65b7e71
alexanderbeletsky <alexander.beletsky@gmail.com>
2014-05-19 20:11:01 +0300

    using mocha reporters

--- source/machiatto.js
+++ source/machiatto.js
@@ -1,23 +1,43 @@
-function expect(expected) {
+var EventEmitter = require('events').EventEmitter;
+var Reporter = require('./reporters/dot');
+
+function expect(runner, context) {
 	function assert(con, message) {
-		if (con) {
-			console.error(message);
+		var test = {
+			slow: function () {
+				return 100;
+			}
+		};
+
+		if (!con) {
+			runner.emit('fail', test);
+		} else {
+			runner.emit('pass', test);
 		}
+
+		runner.emit('test end');
 	}
 
 	return function (provided) {
-		expected = expected || provided;
+		var expected = context.result || provided;
 
 		return {
 			equal: function (actual) {
-				assert(expected !== actual, 'expected: ' + expected + ' actual: ' + actual);
+				assert(expected === actual, 'expected: ' + expected + ' actual: ' + actual);
 			}
 		};
 	};
 }
 
+function reporter(runner) {
+	return new Reporter(runner);
+}
+
 function machiatto(spec) {
+	var runner = new EventEmitter();
 	var context = { _func: {} };
+
+	reporter(runner);
 
 	function reusable(name, fn) {
 		if (fn) {
@@ -38,10 +58,12 @@ function machiatto(spec) {
 	}
 
 	function apply(f, fn) {
-		fn(context, f === 'then' ? expect(context.result) : done);
+		fn(context, f === 'then' ? expect(runner, context) : done);
 	}
 
 	var _machiatto = {};
+
+	runner.emit('start');
 
 	['given', 'when', 'then'].forEach(function (f) {
 		_machiatto[f] = function (name, fn) {
