55d04e008aa21b429c937f4b311e8d9716b0f19d
Vitaly Domnikov <dotcypress@gmail.com>
2014-05-27 12:09:49 +0300

    swap context parameter with function context

--- test/calc.spec.js
+++ test/calc.spec.js
@@ -5,60 +5,60 @@ var spec = machiatto('calc spec');
 var calc = require('./calc.js');
 
 spec('calculating sum')
-	.when('given numbers', function (context) {
-		context.a = 1;
-		context.b = 2;
+	.when('given numbers', function () {
+		this.a = 1;
+		this.b = 2;
 	})
 
-	.and('add operation', function (context) {
-		context.result = calc.add(context.a, context.b);
+	.and('add operation', function () {
+		this.result = calc.add(this.a, this.b);
 	})
 
-	.should('calculate sum', function (context) {
-		expect(context.result).to.equal(3);
+	.should('calculate sum', function () {
+		expect(this.result).to.equal(3);
 	})
 
-	.should('calculate sum 2', function (context) {
-		expect(context.result).to.equal(3);
+	.should('calculate sum 2', function () {
+		expect(this.result).to.equal(3);
 	});
 
 spec('calculating another sum with xand')
-	.when('given another numbers', function (context) {
-		context.a = 3;
-		context.b = 4;
+	.when('given another numbers', function () {
+		this.a = 3;
+		this.b = 4;
 	})
 
-	.xand('add operation', function (context) {
-		context.result = calc.add(context.a, context.b);
+	.xand('add operation', function () {
+		this.result = calc.add(this.a, this.b);
 	})
 
-	.should('calculate sum', function (context) {
-		expect(context.result).to.equal(undefined);
+	.should('calculate sum', function () {
+		expect(this.result).to.equal(undefined);
 	});
 
 spec('calculating another sum with xshould')
-	.when('given another numbers', function (context) {
-		context.a = 5;
-		context.b = 6;
+	.when('given another numbers', function () {
+		this.a = 5;
+		this.b = 6;
 	})
 
 	.and('add operation')
 
-	.xshould('calculate sum', function (context) {
-		expect(context.result).to.equal(11);
+	.xshould('calculate sum', function () {
+		expect(this.result).to.equal(11);
 	});
 
 
 spec('calculating another sum with xwhen')
-	.xwhen('given another numbers', function (context) {
-		context.a = 5;
-		context.b = 6;
+	.xwhen('given another numbers', function () {
+		this.a = 5;
+		this.b = 6;
 	})
 
 	.and('add operation')
 
-	.should('calculate sum', function (context) {
-		expect(isNaN(context.result)).to.be(true);
+	.should('calculate sum', function () {
+		expect(isNaN(this.result)).to.be(true);
 	});
 
 module.exports = spec;
