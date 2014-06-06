5a7c4f68ba79bef141dd9f694fe9063b180691cd
alexanderbeletsky <alexander.beletsky@gmail.com>
2014-06-06 08:37:16 +0300

    strip performance line

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,19 @@ var gulp = require('gulp');
 var exec = require('gulp-exec');
 var diff = require('gulp-diff').diff;
 var reporter = require('gulp-diff').reporter;
+var strip = require('gulp-strip-line');
 
 gulp.task('approve', function () {
 	gulp.src('./bin/machiatto')
 		.pipe(exec('<%= file.path %>', {pipeStdout: true}))
+		.pipe(strip(/\(\d+ ms\)/))
 		.pipe(gulp.dest('./.approved'));
 });
 
 gulp.task('test', function (cb) {
 	gulp.src('./bin/machiatto')
 		.pipe(exec('<%= file.path %>', {pipeStdout: true}))
+		.pipe(strip(/\(\d+ ms\)/))
 		.pipe(diff('./.approved'))
 		.pipe(reporter());
 });
